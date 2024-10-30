9ec63c9242ceb2203b5ae1188508623c6b864c8c
Mimu <124642469+mulhamdi@users.noreply.github.com>
2024-10-30 16:17:02 +0700

    feat: CI4 Shield default setup

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -6,8 +6,8 @@ use CodeIgniter\Config\BaseConfig;
 
 class Email extends BaseConfig
 {
-    public string $fromEmail  = '';
-    public string $fromName   = '';
+    public string $fromEmail  = 'moh.ilhammulhamdi@gmail.com';
+    public string $fromName   = 'Mimu';
     public string $recipients = '';
 
     /**
